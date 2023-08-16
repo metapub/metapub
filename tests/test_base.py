4c3d49200a7d855af00e974366805ae90bbfc099
Matthias Berth <matthias@matthiasberth.com>
2023-08-16 11:33:09 +0200

    Fix for truncated abstract text
    Make sure that the contents of BookDocument/Abstract/AbstractText and Article/Abstract/AbstractText get extracted completely. Previously, it got truncated at the first embedded XML tag, e.g. "Vitamin D<sub>3</sub> effects" would be truncated to "Vitamin D".

--- tests/test_base.py
+++ tests/test_base.py
@@ -2,8 +2,9 @@ from __future__ import absolute_import, unicode_literals
 
 import unittest
 import os
+from lxml.html import fromstring
 
-from metapub.base import parse_elink_response
+from metapub.base import MetaPubObject, parse_elink_response
 
 from .common import CWD
 
@@ -23,3 +24,32 @@ class TestConversions(unittest.TestCase):
         ids = parse_elink_response(fixture)
         assert len(ids) == 0
 
+    
+    def test_clean_html(self):
+        obj = MetaPubObject('<br/>')
+        test_cases = [
+            ('<a href="#">Link</a> <i>italic</i> <b>bold</b> <em>emphasis</em> text', 'Link italic bold emphasis text'),
+            ('<a href="#"><i>italic link</i></a> text', 'italic link text'),
+            ('plain text', 'plain text'),
+            ('Vitamin D<sub>3</sub> m<sup>2</sup>', 'Vitamin D<sub>3</sub> m<sup>2</sup>')
+        ]
+        for html, expected_output in test_cases:
+            actual_output = obj._clean_html(fromstring(f'<div>{html}</div>'))
+            self.assertEqual(actual_output, expected_output)
+
+    def test_extract_text(self):
+        obj = MetaPubObject('<br/>')
+        self.assertEqual(obj._extract_text(None), None)
+        test_cases = [
+            ('<a href="#">Link</a>', 'Link'),
+            ('<i>italic</i>', 'italic'),
+            ('<b>bold</b>', 'bold'),
+            ('<em>emphasis</em>', 'emphasis'),
+            ('plain text', 'plain text'),
+            ('Vitamin D<sub>3</sub> m<sup>2</sup>', 'Vitamin D<sub>3</sub> m<sup>2</sup>')
+            
+        ]
+        for html, expected_output in test_cases:
+            actual_output = obj._extract_text(fromstring(f'<div>{html}</div>'))
+            self.assertEqual(actual_output, expected_output)
+    

76cb03ba3f0515441f3e003223653c0b8f3ef407
Naomi Most <naomi@nthmost.com>
2024-06-27 23:07:37 -0700

    rebase conflicts addressed

--- setup.py
+++ setup.py
@@ -1,28 +1,30 @@
-import glob, os
+import glob
+import os
 
 from setuptools import setup, find_packages
 
 setup(
-    name = 'metapub',
-    version = '0.5.5',
-    description = 'Pubmed / NCBI / eutils interaction library, handling the metadata of pubmed papers.',
-    long_description = open('README.rst').read(), 
-    long_description_content_type = 'text/x-rst',
-    url = 'https://github.com/metapub/metapub',
-    author = 'Naomi Most',
-    maintainer = 'Naomi Most',
-    author_email = 'naomi@nthmost.com',
-    maintainer_email = 'naomi@nthmost.com',
-    license = 'Apache 2.0',
-    packages = find_packages(),
-    entry_points = { 'console_scripts': [
-                        'pubmed_article = metapub.pubmedfetcher_cli:main',
-                        'convert = metapub.convert:main',
-                     ]
-                   },
-    install_requires = [
+    name='metapub',
+    version='0.5.5',
+    description='Pubmed / NCBI / eutils interaction library, handling the metadata of pubmed papers.',
+    long_description=open('README.rst').read(),
+    long_description_content_type='text/x-rst',
+    url='https://github.com/metapub/metapub',
+    author='Naomi Most',
+    maintainer='Naomi Most',
+    author_email='naomi@nthmost.com',
+    maintainer_email='naomi@nthmost.com',
+    license='Apache 2.0',
+    packages=find_packages(),
+    entry_points={'console_scripts': [
+        'pubmed_article = metapub.pubmedfetcher_cli:main',
+        'convert = metapub.convert:main',
+    ]
+    },
+    install_requires=[
         'setuptools',
         'lxml',
+        'lxml_html_clean',
         'requests',
         'eutils',
         'habanero',
@@ -35,5 +37,5 @@ setup(
         'pytest',
         'coloredlogs',
         'python-Levenshtein',
-        ],
-    )
+    ],
+)

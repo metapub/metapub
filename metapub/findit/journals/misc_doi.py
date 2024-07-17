e9ca7d0a40f07593eccb290834a10297aa7bb4a0
Naomi Most <naomi@nthmost.com>
2024-07-17 01:19:40 -0700

    misc_doi branch converted to publisher_lists

--- metapub/findit/journals/misc_doi.py
+++ metapub/findit/journals/misc_doi.py
@@ -52,6 +52,8 @@ simple_formats_doi = {
     'Vector Borne Zoonotic Dis': doi_templates['liebert'],
 
     'N Engl J Med':  'http://www.nejm.org/doi/pdf/{a.doi}',
+    # TODO: 'http://www.bioone.org/doi/pdf/{a.doi}',
+    #        http://www.bioone.org/action/showPublications?type=byAlphabet
 
 }
 
@@ -78,73 +80,9 @@ def load_journals_from_file(publisher):
 
 
 
-for key in doi_templates:
-    load_journals_from_file(key)
-
-
-
-simple_formats_doi = {
-    'Acta Oncol': doi_templates['informa'],
-    'Ann Hum Biol': doi_templates['informa'],
-    'Hemoglobin': doi_templates['informa'],
-    'J Matern Fetal Neonatal Med': doi_templates['informa'],
-    'Ophthalmic Genet': doi_templates['informa'],
-    'Platelets': doi_templates['informa'],
-    'Ren Fail': doi_templates['informa'],
-    'Scand J Rheumatol': doi_templates['informa'],
-    'Scand J Urol Nephrol': doi_templates['informa'],
-    'Xenobiotica': doi_templates['informa'],
-
-    'Anal Chem': doi_templates['acs'],
-    'ACS Appl Mater': doi_templates['acs'],
-    'ACS Chem Biol': doi_templates['acs'],
-    'ACS Nano': doi_templates['acs'],
-    'Biochemistry': doi_templates['acs'],
-    'Chem Res Toxicol': doi_templates['acs'],
-    'Inorg Chem': doi_templates['acs'],
-    'J Agric Food Chem': doi_templates['acs'],
-    'J Am Chem Soc': doi_templates['acs'],
-    'J Comb Chem': doi_templates['acs'],
-    'J Med Chem': doi_templates['acs'],
-    'J Phys Chem A': doi_templates['acs'],
-    'Langmuir': doi_templates['acs'],
-    'Nano Lett': doi_templates['acs'],
-
-
-    # TODO: 'http://www.bioone.org/doi/pdf/{a.doi}',
-    #        http://www.bioone.org/action/showPublications?type=byAlphabet
-
-    'Acta Orthop Scand': doi_templates['taylor_francis'],
-    'Acta Orthop Scand Suppl': doi_templates['taylor_francis'],
-    'Acta Otolaryngol': doi_templates['taylor_francis'],
-    'Acta Otolaryngol Suppl': doi_templates['taylor_francis'],
-    'Acta Otorhinolaryngol Belg': doi_templates['taylor_francis'],
-    'Autophagy': doi_templates['taylor_francis'],
-    'Biosci Biotechnol Biochem': doi_templates['taylor_francis'],
-    'Cancer Biol Ther': doi_templates['taylor_francis'],
-    'Cell Cycle': doi_templates['taylor_francis'],
-    'Crit Rev Food Sci Nutr': doi_templates['taylor_francis'],
-    'Drug Metab Rev': doi_templates['taylor_francis'],
-    'Environ Technol': doi_templates['taylor_francis'],
-    'Health Commun': doi_templates['taylor_francis'],
-    'J Biomol Struct Dyn': doi_templates['taylor_francis'],
-    'J Pers Assess': doi_templates['taylor_francis'],
-    'Pediatr Hematol Oncol': doi_templates['taylor_francis'],
-    'Worm': doi_templates['taylor_francis'],
-
-    'PLoS Biol': doi_templates['plos'],
-    'PLoS Comput Biol': doi_templates['plos'],
-    'PLoS Genet': doi_templates['plos'],
-    'PLoS Med': doi_templates['plos'],
-    'PLoS ONE': doi_templates['plos'],
-    'PLoS Pathog': doi_templates['plos'],
-}
-
-
-
 # Ingest all of the journals contained in the publisher_list/*.txt files.  
 # Add them into the directory according to the templates they require.
-
-
+for key in doi_templates:
+    load_journals_from_file(key)
 
 

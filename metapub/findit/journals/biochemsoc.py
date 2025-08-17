5cb87a93c8b117dbc66d1f382e7a8cb0e53e7533
nthmost <naomi@nthmost.com>
2025-08-17 05:46:54 +0000

    removing orphaned py-based config file

--- metapub/findit/journals/biochemsoc.py
+++
@@ -1,5 +0,0 @@
-# Portlan Press Biochemical Society journals: mostly VIP
-biochemsoc_format = 'http://{host}/content/{ja}/{a.volume}/{a.issue}/{a.first_page}.full.pdf'
-biochemsoc_journals = {'Biochem J': {'host': 'www.biochemj.org', 'ja': 'ppbiochemj'},
-                       'Clin Sci': {'host': 'www.clinsci.org', 'ja': 'ppclinsci'},
-                       }
